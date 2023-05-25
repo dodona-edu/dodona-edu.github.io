cc220f386a07c6311d8ae0f1352ccdd59b6264a8
Niko Strijbol <strijbol.niko@gmail.com>
2023-05-25 14:39:57 +0200

    Support image zoom

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,4 +1,25 @@
 import DefaultTheme from 'vitepress/theme';
+import { onMounted, watch, nextTick } from 'vue';
+import { useRoute } from 'vitepress';
+import mediumZoom from 'medium-zoom';
 import './custom.css';
 
-export default DefaultTheme;
+// Image zoom taken from https://github.com/vuejs/vitepress/issues/854
+
+export default {
+  ...DefaultTheme,
+  setup() {
+    const route = useRoute();
+    const initZoom = () => {
+      mediumZoom('main :not(a) img', { background: 'var(--vp-c-bg)' });
+    };
+
+    onMounted(() => {
+      initZoom();
+    });
+    watch(
+        () => route.path,
+        () => nextTick(() => initZoom())
+    );
+  }
+};
