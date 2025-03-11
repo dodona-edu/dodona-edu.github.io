b8aaaaa22829f86f7c8c7074f42d1061afbd1559
B. Willems <Brecht.Willems@UGent.be>
2025-03-11 14:19:24 +0100

    Update Dodona links 2

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -101,7 +101,8 @@ export default defineConfig({
     },
 
     socialLinks: [
-      { icon: 'twitter', link: 'https://x.com/DodonaEdu' },
+      { icon: 'x', link: 'https://x.com/DodonaEdu' },
+      { icon: 'bluesky', link: 'https://bsky.app/profile/dodona.be' },
       { icon: 'github', link: 'https://github.com/dodona-edu' }
     ]
   },
